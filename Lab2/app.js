0e24b0714c66b06f40322190501c6daa72648650
Max <rendon4ik@gmail.com>
2022-10-31 20:36:44 +0200

    Fixed naming for js file

--- Lab2/app.js
+++ Lab2/app.js
@@ -2,13 +2,13 @@ const http = require('http');
 
 const PORT = 80;
 
-const listenter = (_req, res) => {
+const listener = (_req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Some text here');
 };
 
-const server = http.createServer(listenter);
+const server = http.createServer(listener);
 server.listen(PORT, () => {
   console.log(`Server running`);
 });
